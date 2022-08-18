481359f503c47f06c32eb35afd3282c82140cd90
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-08-18 14:51:57 -0300

    First Commit

---
+++ src/main/java/com/mycompany/todoapp/TodoApp.java
@@ -0,0 +1,12 @@
+package com.mycompany.todoapp;
+
+/**
+ *
+ * @author vitor
+ */
+public class TodoApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
